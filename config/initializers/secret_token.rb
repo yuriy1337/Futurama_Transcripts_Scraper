537f311ee5bfef4b62dcba44155617cbefc7ac5e
Yuriy <yuriy1337@gmail.com>
2011-09-22 18:42:41 -0500

    Adding initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FuturamaTranscriptsScraper::Application.config.secret_token = 'b7790c11dc08b55435b25fcd99b4b66ebb14223f5662e7833d3bc004cfc157fcc155cc8d86d20ce17db31b656cba2643fe8591d2edb8396d018fce2296d460b1'
